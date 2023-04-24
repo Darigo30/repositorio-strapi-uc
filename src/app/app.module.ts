3cb5e380da1c1722f6a44a518ce0952d33a2d9cb
Dariana <dariana.gomez@uc.cl>
2023-04-24 14:23:13 -0400

    se agrega header y footer y funcionalidad de componente carrusel

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,26 @@
 import { NgModule } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CarruselComponent } from './componentes/carrusel/carrusel.component';
+import '@angular/localize/init';
+import { HeaderComponent } from './componentes/header/header.component';
+import { FooterComponent } from './componentes/footer/footer.component'
+
 @NgModule({
   declarations: [
     AppComponent,
-    CarruselComponent
+    CarruselComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'serverApp' }),
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
