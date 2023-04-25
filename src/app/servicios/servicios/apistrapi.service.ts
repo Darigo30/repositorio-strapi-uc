2001423d0275cfca3dd4f2f94c56cfd042c8a1e3
Dariana <dariana.gomez@uc.cl>
2023-04-25 14:23:51 -0400

    mejora en funcion que retorna las conf del header

--- src/app/servicios/servicios/apistrapi.service.ts
+++ src/app/servicios/servicios/apistrapi.service.ts
@@ -10,17 +10,23 @@ import axios from 'axios';
 export class ApistrapiService {
 
   constructor(private http: HttpClient) { }
-   
+
+   //funcion que retorne la conf de headers
+    getHeaders(configH: any) {
+     return configH = {
+      headers: {
+        Authorization: 'Bearer ' + '05cf225a113170e4dcd516df74a2d37ff1d4256d0c568799aa1c5c5277d76acd1ab7b4752fc68561744f7d36984a468a21a64fffac47e90e3781ed2df2dfb7e672b8cdd045705a334eeb6c386d954daee90a0367eb39285ca8ed76f1cdb3b947a256a0fe22240c6397ed62ecb5bfaae48c2acf4ec8eb83045fffafe9791fbd4f'
+       }
+     }
+    }
+
+
   //Obtengo API Slider
     async getStrapiSlider() { 
-      const conf = {
-        headers: {
-         Authorization: 'Bearer ' + '05cf225a113170e4dcd516df74a2d37ff1d4256d0c568799aa1c5c5277d76acd1ab7b4752fc68561744f7d36984a468a21a64fffac47e90e3781ed2df2dfb7e672b8cdd045705a334eeb6c386d954daee90a0367eb39285ca8ed76f1cdb3b947a256a0fe22240c6397ed62ecb5bfaae48c2acf4ec8eb83045fffafe9791fbd4f'
-        }
-      }
-      const responseSliders = await axios.get('http://localhost:1337/api/sliders?populate=*', conf)
+      const responseSliders = await axios.get('http://localhost:1337/api/sliders?populate=*', this.getHeaders(''))
         try {
           const dataStrapiSliders = responseSliders.data;
+          console.log("get headers", this.getHeaders(''))
           return dataStrapiSliders;
         } catch (error) {
           console.log(error);
@@ -29,12 +35,7 @@ export class ApistrapiService {
 
     //Obtengo API Noticias
     async getStrapiNoticias() { 
-      const conf = {
-        headers: {
-         Authorization: 'Bearer ' + '05cf225a113170e4dcd516df74a2d37ff1d4256d0c568799aa1c5c5277d76acd1ab7b4752fc68561744f7d36984a468a21a64fffac47e90e3781ed2df2dfb7e672b8cdd045705a334eeb6c386d954daee90a0367eb39285ca8ed76f1cdb3b947a256a0fe22240c6397ed62ecb5bfaae48c2acf4ec8eb83045fffafe9791fbd4f'
-        }
-      }
-      const reponseNoticias = await axios.get('http://localhost:1337/api/noticias?populate=*', conf)
+      const reponseNoticias = await axios.get('http://localhost:1337/api/noticias?populate=*', this.getHeaders(''))
         try {
           const dataStrapiNoticias = reponseNoticias.data;
           return dataStrapiNoticias;
